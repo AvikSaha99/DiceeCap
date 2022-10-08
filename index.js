99aa5ad5e01a261c43776a02316d276bf2f826dd
AvikSaha99 <73651533+AvikSaha99@users.noreply.github.com>
2022-10-08 20:44:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var RandNum1 = Math.floor(Math.random()*6)+1;
+var RandImg1 = "images/" + "dice" + RandNum1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", RandImg1);
+
+
+var RandNum2 = Math.floor(Math.random()*6)+1;
+var RandImg2= "images/" + "dice" + RandNum2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", RandImg2);
+
+if(RandNum1 >RandNum2)
+{
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}
+else if(RandNum1 < RandNum2)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins";
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw";
+}
+
